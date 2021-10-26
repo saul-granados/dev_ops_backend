0098bc589439345344e2ff4c707878a40b9f029d
Saul Granados Rocha <saul.granados@cominvi.com.mx>
2021-10-26 18:03:02 -0500

    Pruebas unitarias

--- src/main/java/com/paymentchain/billing/dto/InvoiceResponse.java
+++ src/main/java/com/paymentchain/billing/dto/InvoiceResponse.java
@@ -14,16 +14,57 @@ import lombok.Data;
  * @author sotobotero
  */
 @Data
-@ApiModel(description = "This model represent a Invoice data that user should send on a request on post method" )
+@ApiModel(description = "This model represent a Invoice data that user should send on a request on post method")
 public class InvoiceResponse {
-    @ApiModelProperty(name = "invoiceId", required = true,example = "2548975",value = "Unique Id of invoice after it's created")
-   private long invoiceId;
-     @ApiModelProperty(name = "customer", required = true,example = "1", value = "Unique Id of customer taht represent the owner of invoice")
-   private long customer;
-      @ApiModelProperty(name = "number", required = true,example = "TFS2548975",value = "Bussines number that identified a invoice",allowEmptyValue = false)
-   private String number;
-       @ApiModelProperty(name = "detail", required = false,example = "Professional services")
-   private String detail;
-        @ApiModelProperty(name = "amount", required = true,example = "3659.23")
-   private double amount; 
+	@ApiModelProperty(name = "invoiceId", required = true, example = "2548975", value = "Unique Id of invoice after it's created")
+	private long invoiceId;
+	@ApiModelProperty(name = "customer", required = true, example = "1", value = "Unique Id of customer taht represent the owner of invoice")
+	private long customer;
+	@ApiModelProperty(name = "number", required = true, example = "TFS2548975", value = "Bussines number that identified a invoice", allowEmptyValue = false)
+	private String number;
+	@ApiModelProperty(name = "detail", required = false, example = "Professional services")
+	private String detail;
+	@ApiModelProperty(name = "amount", required = true, example = "3659.23")
+	private double amount;
+
+	public long getInvoiceId() {
+		return invoiceId;
+	}
+
+	public void setInvoiceId(long invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+
+	public long getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(long customer) {
+		this.customer = customer;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
 }
--- src/main/java/com/paymentchain/billing/entities/Invoice.java
+++ src/main/java/com/paymentchain/billing/entities/Invoice.java
@@ -11,8 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Data;
 
-
-
 /**
  *
  * @author sotobotero
@@ -20,11 +18,52 @@ import lombok.Data;
 @Entity
 @Data
 public class Invoice {
-   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-   private long id;
-   private long customerId;
-   private String number;
-   private String detail;
-   private double amount;  
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private long customerId;
+	private String number;
+	private String detail;
+	private double amount;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
 }
